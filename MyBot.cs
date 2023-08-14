e6a57b59c748d17db61c307cd999daa55ccd2322
mcngnt <martin.cuingnet@icloud.com>
2023-08-14 14:13:44 +0200

    Simplifying whithout draw check

--- MyBot.cs
+++ MyBot.cs
@@ -1,13 +1,10 @@
 ﻿using ChessChallenge.API;
 using System;
-using System.Linq;
-using static ChessChallenge.Example.EvilBot;
+using static System.Formats.Asn1.AsnWriter;
 
 public class MyBot : IChessBot
 {
-
-
-
+    // None, Pawn, Knight, Bishop, Rook, Queen, King
     int[] pieceValues = { 0, 100, 320, 330, 500, 900, 20000 };
 
     int[,] tables = {{0,  0,  0,  0,
@@ -64,20 +61,44 @@ public class MyBot : IChessBot
                          20, 30, 10,  0} };
 
     Move bestMove;
-    Move secondBestMove;
     bool hasNotFinished;
-    int maxTime = 500;
+    int maxTime = 100;
     int posNB;
     Move finalMove;
-    Move secondFinalMove;
-
-    // int transpoNB;
 
 
     public Move Think(Board board, Timer timer)
     {
 
         //Console.WriteLine(Eval(Board.CreateBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")));
+
+       /* PieceList[] plists = board.GetAllPieceLists();
+
+
+        foreach (PieceList plist in plists)
+        {
+            for (int i = 0; i < plist.Count; i++)
+            {
+                Piece p = plist.GetPiece(i);
+
+
+                Console.WriteLine("Piece Color : " + p.IsWhite);
+                Console.WriteLine("Piece Type : " + p.PieceType);
+                Console.WriteLine("Value : " + tables[(int)p.PieceType - 1, (p.Square.File >= 4 ? 7 - p.Square.File : p.Square.File) + 4 * (p.IsWhite ? 7 - p.Square.Rank : p.Square.Rank)]);
+
+            }
+        }
+
+        int pieceNumber = 0;
+
+        foreach (PieceList plist in plists)
+        {
+            pieceNumber += plist.Count;
+        }
+        // 0 at first and 1 in endgame
+        float endGameCoef = 1 - (pieceNumber / 32);
+
+        Console.WriteLine("EndGameCoef : " + endGameCoef);*/
 
         float lastEval = 0;
 
@@ -89,14 +110,13 @@ public class MyBot : IChessBot
         {
             hasNotFinished = false;
             posNB = 0;
-            float eval = Search(board, float.NegativeInfinity, float.PositiveInfinity, i, i, false, timer);
+            float eval = Search(board, float.NegativeInfinity, float.PositiveInfinity, i, i, timer);
 
             if (!hasNotFinished)
             {
                 Console.WriteLine(posNB);
                 lastEval = eval;
                 finalMove = bestMove;
-                secondFinalMove = secondBestMove;
                 finalDepth += 1;
             }
             else
@@ -105,6 +125,8 @@ public class MyBot : IChessBot
             }
         }
 
+
+
         //Console.WriteLine("------");
 
         //Console.WriteLine(lastEval);
@@ -112,79 +134,28 @@ public class MyBot : IChessBot
 
         //Console.WriteLine("*------*");
 
-        board.MakeMove(finalMove);
-        if (board.IsDraw() && lastEval > -50 && !secondFinalMove.Equals(Move.NullMove))
-        {
-            board.UndoMove(finalMove);
-            return secondFinalMove;
-        }
-        board.UndoMove(finalMove);
 
         return finalMove;
     }
 
-    public float Search(Board board, float alpha, float beta, int depth, int startingDepth, bool isQuiet, Timer timer)
+    public float QuiescneceSearch(Board board, float alpha, float beta)
     {
-        if (!isQuiet)
+        float currentEval = Eval(board);
+        if (currentEval >= beta)
         {
-
-            if (depth == 0)
-            {
-                posNB += 1;
-                return Search(board, alpha, beta, depth, startingDepth, true, timer);
-            }
-
-            if (board.IsInCheckmate())
-            {
-                return -20000;
-            }
-
-            if (timer.MillisecondsElapsedThisTurn > maxTime)
-            {
-                hasNotFinished = true;
-                return 0;
-            }
+            return beta;
         }
-        else
+        if (currentEval > alpha)
         {
-            float currentEval = Eval(board);
-            if (currentEval >= beta)
-            {
-                return beta;
-            }
-            if (currentEval > alpha)
-            {
-                alpha = currentEval;
-            }
+            alpha = currentEval;
         }
 
-
-        Move[] moves = board.GetLegalMoves(isQuiet);
-
-        if (!isQuiet)
-        {
-            float score(Move move)
-            {
-                return (move.Equals(finalMove) ? 9999f : 0f) + (move.IsPromotion ? pieceValues[(int)move.PromotionPieceType] : 0f) + (move.IsCapture ? (pieceValues[(int)board.GetPiece(move.TargetSquare).PieceType] - pieceValues[(int)board.GetPiece(move.StartSquare).PieceType]) : 0);
-            }
-
-            int comp(Move move1, Move move2)
-            {
-                return -score(move1).CompareTo(score(move2));
-            }
-
-            Array.Sort(moves, comp);
-        }
-
-
-        Move currentBestMove = Move.NullMove;
-        Move secondCurrentBestMove = Move.NullMove;
+        Move[] moves = board.GetLegalMoves(true);
 
         foreach (var move in moves)
         {
             board.MakeMove(move);
-            int extension = board.IsInCheck() ? 1 : 0;
-            float eval = -Search(board, -beta, -alpha, depth - 1 + extension, startingDepth + extension, isQuiet, timer);
+            float eval = -QuiescneceSearch(board, alpha, beta);
             board.UndoMove(move);
 
             if (eval >= beta)
@@ -194,15 +165,72 @@ public class MyBot : IChessBot
             if (eval > alpha)
             {
                 alpha = eval;
-                secondCurrentBestMove = currentBestMove;
+            }
+        }
+
+        return alpha;
+    }
+
+
+    public float Search(Board board, float alpha, float beta, int depth, int startingDepth, Timer timer)
+    {
+
+        if (depth == 0)
+        {
+            posNB += 1;
+            return QuiescneceSearch(board, alpha, beta);
+        }
+
+        if (board.IsInCheckmate())
+        {
+            return -20000;
+        }
+
+        if (timer.MillisecondsElapsedThisTurn > maxTime)
+        {
+            hasNotFinished = true;
+            return 0;
+        }
+
+
+        Move[] moves = board.GetLegalMoves(false);
+
+        float score(Move move)
+        {
+            return (move.Equals(finalMove) ? 9999f : 0f) + (move.IsPromotion ? pieceValues[(int)move.PromotionPieceType] : 0f) + (move.IsCapture ? (pieceValues[(int)board.GetPiece(move.TargetSquare).PieceType] - pieceValues[(int)board.GetPiece(move.StartSquare).PieceType]) : 0);
+        }
+
+        int comp(Move move1, Move move2)
+        {
+            return -score(move1).CompareTo(score(move2));
+        }
+
+        Array.Sort(moves, comp);
+
+
+        Move currentBestMove = Move.NullMove;
+
+        foreach (var move in moves)
+        {
+            board.MakeMove(move);
+            int extension = board.IsInCheck() ? 1 : 0;
+            float eval = -Search(board, -beta, -alpha, depth - 1 + extension, startingDepth + extension, timer);
+            board.UndoMove(move);
+
+            if (eval >= beta)
+            {
+                return beta;
+            }
+            if (eval > alpha)
+            {
+                alpha = eval;
                 currentBestMove = move;
             }
         }
 
-        if (!isQuiet && depth == startingDepth)
+        if (depth == startingDepth)
         {
             bestMove = currentBestMove;
-            secondBestMove = secondCurrentBestMove;
         }
 
         return alpha;
@@ -220,7 +248,9 @@ public class MyBot : IChessBot
         {
             pieceNumber += plist.Count;
         }
-        // 0 at first and 1 in endgame
+
+
+
         float endGameCoef = 1 - (pieceNumber / 32);
 
         foreach (PieceList plist in plists)
@@ -231,7 +261,8 @@ public class MyBot : IChessBot
 
                 int colorMult = board.IsWhiteToMove == p.IsWhite ? 1 : -1;
 
-                score += pieceValues[(int)p.PieceType] * colorMult * 3 + tables[(int)p.PieceType - 1, (p.Square.File >= 4 ? 7 - p.Square.File : p.Square.File) + 4 * (p.IsWhite ? 7 - p.Square.Rank : p.Square.Rank)] * colorMult * (1 - endGameCoef);
+                score += pieceValues[(int)p.PieceType] * colorMult * 3;
+                score += tables[(int)p.PieceType - 1, (p.Square.File >= 4 ? 7 - p.Square.File : p.Square.File) + 4 * (p.IsWhite ? 7 - p.Square.Rank : p.Square.Rank)] * colorMult * (1 - endGameCoef);
 
                 if (p.PieceType == PieceType.King)
                 {
