0acd037b227319bb322bee81a00fae302ff8633b
mcngnt <martin.cuingnet@icloud.com>
2023-08-21 15:06:06 +0200

    Blank

--- MyBot.cs
+++ MyBot.cs
@@ -18,6 +18,7 @@ public class MyBot : IChessBot
                           94, 281, 297, 512, 936, 0 };
     int[] piecePhaseValue = { 0, 1, 1, 2, 4, 0 };
 
+
     int[,] pieceTable = { { 0,   0,   0,   0,   0,   0,  0,   0,
      98, 134,  61,  95,  68, 126, 34, -11,
      -6,   7,  26,  31,  65,  56, 25, -20,
