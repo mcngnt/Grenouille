f986757e99d763f21c71241f03172ceee7ec2768
mcngnt <martin.cuingnet@icloud.com>
2023-08-25 23:28:02 +0200

    check malus eval

--- MyBot.cs
+++ MyBot.cs
@@ -231,7 +231,7 @@ public class MyBot : IChessBot
             }
         }
 
-        return (middleGame * gamePhase + endGame * (24 - gamePhase)) / 24 * (board.IsWhiteToMove ? 1 : -1) + gamePhase / 2;
+        return (middleGame * gamePhase + endGame * (24 - gamePhase)) / 24 * (board.IsWhiteToMove ? 1 : -1) + gamePhase / 2 - (board.IsInCheck() ? 10 : 0);
 
     }
 
